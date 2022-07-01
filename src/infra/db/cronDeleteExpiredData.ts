3379216c9d71132f8b80508b3b75f2a237f12b4c
manten120 <hamaw8888@gmail.com>
2022-07-01 14:38:50 +0900

    期限切れのtempUserとforgottenUserを削除するタイミングを0時0分に変更

--- src/infra/db/cronDeleteExpiredData.ts
+++ src/infra/db/cronDeleteExpiredData.ts
@@ -3,17 +3,9 @@ import { sequelize } from './sequelize-loader';
 
 export const cronDeleteExpiredData = () => {
   cron.schedule(
-    '0,30 * * * *',
-    () => {
-      sequelize.query(`DELETE FROM forgotten_users WHERE expired_at < "${new Date(Date.now()).toISOString()}"`);
-    },
-    {
-      timezone: 'Asia/Tokyo',
-    }
-  );
-  cron.schedule(
     '0 0 * * *',
     () => {
+      sequelize.query(`DELETE FROM forgotten_users WHERE expired_at < "${new Date(Date.now()).toISOString()}"`);
       sequelize.query(`DELETE FROM temp_users WHERE expired_at < "${new Date(Date.now()).toISOString()}"`);
     },
     {
