4679da7b4dc9a7815a1dea4eb07d742e81687b54
manten120 <hamaw8888@gmail.com>
2022-07-01 09:14:26 +0900

    トッページを整えた

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,8 @@ const router = express.Router();
 
 router.get('/', (req, res, _next) => {
   res.render('index', {
-    title: '「ログイン後の画面」以外のURLで表示される画面',
+    title: 'トッページ',
+    message: '「ログイン後の画面」以外のURLで表示される画面',
     loggedIn: req.session.loggedIn,
     userName: req.session.userName,
   });
